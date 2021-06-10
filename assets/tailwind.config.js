22c740137de4ba1af4d5f112ec5a094834a0d265
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2021-06-10 14:03:07 +0200

    deactivate jit

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,13 +1,10 @@
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: [
-    "../lib/**/*.eex",
-    "../lib/**/*.leex",
-    "../lib/**/*_view.ex",
-    "../lib/**/views/*.ex"
+    '../lib/**/*.eex',
+    '../lib/**/*.leex',
+    '../lib/**/*_view.ex',
+    '../lib/**/views/*.ex',
   ],
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
-  ]
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
