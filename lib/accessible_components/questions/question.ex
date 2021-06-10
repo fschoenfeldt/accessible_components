07cda62e63c38207ccb58f0e5e25f917ee201b4e
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2021-06-10 16:22:21 +0200

    Feature/questions answers database (#1)
    * add question
    
    * add answers & seed
    
    * disable plugins forms and typography
    
    * some styling
    
    * add a11y tests instructions

---
+++ lib/accessible_components/questions/question.ex
@@ -0,0 +1,21 @@
+defmodule AccessibleComponents.Questions.Question do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "questions" do
+    field :title, :string
+    field :type, :string
+    has_many :answers, AccessibleComponents.Answers.Answer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(question, attrs) do
+    question
+    |> cast(attrs, [:title, :type])
+    |> validate_required([:title, :type])
+  end
+end
