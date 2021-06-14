80e01861d2f3b24fa8aa8946482afc488f0eb3f4
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2021-06-14 16:14:15 +0200

    fix js issues

--- assets/js/components/radio_button.js
+++ assets/js/components/radio_button.js
@@ -4,26 +4,13 @@ export const checkRequiredFields = () => {
   // get all required fieldsets
   const requiredFieldsets = $$('[data-required="true"]')
 
+  requiredFieldsets.map(({ classList }) =>
+    classList.remove('question--fillMeOut')
+  )
+
   // get fieldsets that are not filled out yet
-  const fieldsetsRemainingToFill = requiredFieldsets.filter(fieldset => {
-    // TODO: This doesn't work either
-    // fieldset.classList.value = 'question'
-
-    const answerOptions = $$('input', fieldset)
-    const isFilledOut = answerOptions.some(({ checked }) => !checked)
-    return isFilledOut
-  })
-
-  // clear out previous hints
-  // TODO: this doesn't work
-  requiredFieldsets.map(el => {
-    el.classList.remove('question--fillMeOut')
-  })
-
-  // add border class
-  fieldsetsRemainingToFill.forEach(el => {
-    el.classList.add('question--fillMeOut')
-  })
-
-  fieldsetsRemainingToFill |> console.log
+  // and place border class around them
+  const _fieldsetsRemainingToFill = requiredFieldsets
+    .filter(fieldset => $$('input', fieldset).every(({ checked }) => !checked))
+    .forEach(({ classList }) => classList.add('question--fillMeOut'))
 }
